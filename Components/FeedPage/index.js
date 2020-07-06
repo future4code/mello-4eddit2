da353c48d5b3c857a36911bba2963431153e0df8
Fabricio <earthbornshepard@hotmail.com>
2020-07-06 11:53:01 -0300

    template inicial

---
+++ Components/FeedPage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+function FeedPage() {
+  return (
+    <div>
+      <h1>FeedPage</h1>
+    </div>
+  );
+}
+
+export default FeedPage;
